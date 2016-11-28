7e219e45c6e6a2344b4a32e3eb30dd709c8da5aa
ivodvb <ivo@dewebdivisie.nl>
2016-11-28 14:18:53 +0100

    update

--- js/App.js
+++ js/App.js
@@ -1,4 +1,4 @@
-function DatabaseBuilder () {
+function App () {
     this.buildDatabase();
 
     var self = this;
@@ -105,6 +105,10 @@ function DatabaseBuilder () {
             btn.prop('disabled', false);
 
             return false;
+        } else {
+            consoleWindow.writeLineToConsole(
+                'Scanning barcode '+barcode
+            );
         }
 
         self.scanBarcode(barcode, ticketObjectStore, btn);
@@ -157,9 +161,151 @@ function DatabaseBuilder () {
             };
         };
     });
+
+    jQuery('#search').on('click', function () {
+        var btn = jQuery(this);
+        btn.prop('disabled', true);
+
+        var searchQuery = jQuery('#search-query').val();
+
+        consoleWindow.writeLineToConsole('Searching for '+searchQuery);
+
+        searchQuery = searchQuery.toLowerCase();
+
+        if (searchQuery.length < 3) {
+            consoleWindow.writeLineToConsole(
+                'Search query must be at least 3 characters long', 'red'
+            );
+
+            btn.prop('disabled', false);
+
+            return;
+        }
+
+        var singleKeyRange = IDBKeyRange.bound(searchQuery, searchQuery + '\uffff');
+
+        var ticketObjectStore = self.getObjectStore('ticket');
+        var ticketCustomerNameCursor = ticketObjectStore.index('nameKey').openCursor(singleKeyRange, 'prev');
+
+        var searchResultCount = 0;
+
+        ticketCustomerNameCursor.onsuccess = function (event) {
+            var cursor = event.target.result;
+
+            if (cursor) {
+                self.printTicketInfo(cursor.value);
+
+                ++searchResultCount;
+
+                cursor.continue();
+            } else if (searchResultCount === 0) {
+                    var orderKeyCursor = ticketObjectStore.index('orderKey').openCursor(singleKeyRange, 'prev');
+
+                    orderKeyCursor.onsuccess = function (event) {
+                        var cursor = event.target.result;
+
+                        if (cursor) {
+                            self.printTicketInfo(cursor.value);
+
+                            ++searchResultCount;
+
+                             cursor.continue();
+                        } else if (searchResultCount == 0) {
+                            consoleWindow.writeLineToConsole(
+                                'No results round for query "'+searchQuery+'"', 'orange'
+                            );
+                            
+                            btn.prop('disabled', false);
+                        } else {
+                            consoleWindow.writeLineToConsole(
+                                'Finished search with '+searchResultCount+
+                                ' result(s)',
+                                'green'
+                            );
+
+                            btn.prop('disabled', false);
+                        }
+                    };
+
+                    orderKeyCursor.onerror = function () {
+                        consoleWindow.writeLineToConsole(
+                            'Error while loading search results', 'red'
+                        );
+
+                        btn.prop('disabled', false);
+                    };
+            } else {
+                consoleWindow.writeLineToConsole(
+                    'Finished search with '+searchResultCount+' result(s)',
+                    'green'
+                );
+
+                btn.prop('disabled', false);
+            }
+        };
+
+        ticketCustomerNameCursor.onerror = function (event) {
+            consoleWindow.writeLineToConsole(
+                'Error while loading search results', 'red'
+            );
+
+            btn.prop('disabled', false);
+        }
+    });
+
+    jQuery('#search-full').on('click', function () {
+        var btn = jQuery(this);
+        btn.prop('disabled', true);
+
+        var searchQuery = jQuery('#search-query').val();
+
+        if (searchQuery.length < 3) {
+            consoleWindow.writeLineToConsole(
+                'Search query must be at least 3 characters long', 'red'
+            );
+
+            btn.prop('disabled', false);
+
+            return;
+        }
+
+        consoleWindow.writeLineToConsole('Searching for '+searchQuery);
+
+        searchQuery = searchQuery.toLowerCase();
+
+        var ticketObjectStore = self.getObjectStore('ticket');
+        var ticketCursor = ticketObjectStore.openCursor();
+
+        var searchResultsCount = 0;
+
+        ticketCursor.onsuccess = function (event) {
+            var cursor = event.target.result;
+
+            if (cursor) {
+                if (cursor.value.nameKey.indexOf(searchQuery) !== -1
+                    || cursor.value.orderKey.indexOf(searchQuery) !== -1
+                    || cursor.value.barcode.indexOf(searchQuery) !== -1)
+                {
+                    ++searchResultsCount;
+
+                    self.printTicketInfo(cursor.value);
+                }
+
+                cursor.continue();
+            } else {
+                btn.prop('disabled', false);
+
+                consoleWindow.writeLineToConsole(
+                    'Finished search with '+searchResultsCount+' result(s)',
+                    'green'
+                );
+            }
+        };
+    });
 }
 
-DatabaseBuilder.prototype = {
+App.prototype = {
+    appReady: false,
     db: null,
 
     buildDatabase: function () {
@@ -176,23 +322,37 @@ DatabaseBuilder.prototype = {
             return false;
         }
 
-        consoleWindow.writeLineToConsole('Checking database');
+        consoleWindow.writeLineToConsole('Checking database structure...');
 
         var self = this;
 
-        var request = window.indexedDB.open('event', 1);
+        var request = window.indexedDB.open('event', 3);
 
         request.onupgradeneeded = function (event) {
             consoleWindow.writeLineToConsole('Database needs an upgrade');
             consoleWindow.writeLineToConsole('Performing db upgrade');
 
             var db = event.target.result;
-            var objectStore = db.createObjectStore('ticket', {
-                keyPath: 'barcode'
-            });
 
-            objectStore.createIndex('ticketName', 'ticketName', { unique: false });
-            objectStore.createIndex('name', 'name', { unique: false });
+            if (event.oldVersion === 0) {
+                var objectStore = db.createObjectStore('ticket', {
+                    keyPath: 'barcode'
+                });
+
+                objectStore.createIndex('ticketNameKey', 'ticketNameKey', { unique: false });
+                objectStore.createIndex('nameKey', 'nameKey', { unique: false });
+            }
+
+            if (event.oldVersion < 2) {
+                var ticketObjectStore = request.transaction.objectStore('ticket');
+                ticketObjectStore.createIndex('orderKey', 'orderKey', { unique: false });
+            }
+
+            if (event.oldVersion < 3) {
+                db.createObjectStore('statistics', {
+                    keyPath: 'ticketName'
+                });
+            }
         };
 
         request.onerror = function (event) {
@@ -202,22 +362,143 @@ DatabaseBuilder.prototype = {
         };
 
         request.onsuccess = function (event) {
-            consoleWindow.writeLineToConsole(
-                'Database is ready', 'green'
-            );
+            consoleWindow.writeLineToConsole('Validating database...');
 
             self.db = request.result;
+
+            var statisticsObjectStore = self.getObjectStore('statistics', 'readwrite', true);
+            var statisticsCursor = statisticsObjectStore.openCursor();
+            var statisticsRowCount = 0;
+            statisticsCursor.onsuccess = function (event) {
+                var statisticRowCursor = event.target.result;
+
+                if (statisticRowCursor) {
+                    ++statisticsRowCount;
+
+                    console.log(statisticRowCursor.value);
+
+                    statisticRowCursor.continue();
+                } else if (statisticsRowCount === 0) {
+                    consoleWindow.writeLineToConsole(
+                        'Statistics table contents missing, generating it now',
+                        'orange'
+                    );
+
+                    self.buildStatisticsTable().then(function () {
+                        self.appReady = true;
+
+                        consoleWindow.writeLineToConsole(
+                            'Database is ready', 'green'
+                        );
+                    });
+                } else {
+                    self.appReady = true;
+
+                    consoleWindow.writeLineToConsole(
+                        'Database is ready', 'green'
+                    );
+                }
+            };
+
+            statisticsCursor.onerror = function () {
+                consoleWindow.writeLineToConsole(
+                    'Error while checking statistics database, app crashed',
+                    'red'
+                );
+            }
         };
     },
 
-    getObjectStore: function (name, privilege) {
+    buildStatisticsTable: function () {
+        this.appReady = false;
+
+        var self = this;
+
+        return new Promise(function (fulfill, reject) {
+            var ticketObjectStore = self.getObjectStore('ticket', 'readwrite', true);
+            var ticketCursor = ticketObjectStore.index('ticketNameKey').openCursor(null, 'nextunique');
+            var ticketCount = 0;
+
+            var statisticObjects = {};
+
+            ticketCursor.onsuccess = function(event) {
+                console.log('Checking ticket now');
+
+                var ticketRowCursor = event.target.result;
+
+                if (ticketRowCursor) {
+                    ++ticketCount;
+
+                    statisticObjects[ticketRowCursor.value.ticketNameKey] = {
+                        ticketName: ticketRowCursor.value.ticketName,
+                        ticketNameKey: ticketRowCursor.value.ticketNameKey,
+                        scanned: 0,
+                        total: 0
+                    };
+
+                    self.getObjectStore('statistics', 'readwrite', true).add(statisticObjects[ticketRowCursor.value.ticketNameKey]).onsuccess = function () {
+                        console.log('Added '+statisticObjects[ticketRowCursor.value.ticketNameKey]+' to db');
+                    };
+
+                    // todo This should be in the previous add function, but that's not working
+                    ticketRowCursor.continue();
+                } else if (ticketCount === 0) {
+                    self.appReady = true;
+
+                    consoleWindow.writeLineToConsole(
+                        'No tickets found, app is ready', 'green'
+                    );
+
+                    fulfill();
+                } else {
+                    self.appReady = true;
+
+                    console.log(statisticObjects);
+
+                    consoleWindow.writeLineToConsole(
+                        'Finished populating statistics database',
+                        'green'
+                    );
+
+                    fulfill();
+                }
+            };
+
+            ticketCursor.onerror = function () {
+                consoleWindow.writeLineToConsole(
+                    'Error while populating statistics table', 'red'
+                );
+
+                reject();
+            };
+        });
+    },
+
+    getObjectStore: function (name, privilege, force) {
+        if (!this.appReady && !force) {
+            consoleWindow.writeLineToConsole(
+                'Cannot execute DB query, app is not ready yet', 'red'
+            );
+
+            return;
+        }
+
         privilege = privilege || 'readonly';
 
         var transaction = this.db.transaction([name], privilege);
         return transaction.objectStore(name);
     },
 
-    addRow: function (name, ticketName, barcode, scanned, scannedDateTime) {
+    /**
+     * @param {string} name
+     * @param {string} ticketName
+     * @param {string} orderKey
+     * @param {string} barcode
+     * @param {boolean} [scanned]
+     * @param {string} [scannedDateTime]
+     * @returns {*}
+     */
+    addRow: function (name, ticketName, orderKey, barcode, scanned, scannedDateTime) {
         var self = this;
 
         return new Promise(function (fulfill, reject) {
@@ -225,8 +506,11 @@ DatabaseBuilder.prototype = {
             scannedDateTime = scannedDateTime || null;
 
             var ticketObject = {
+                nameKey: name.toLowerCase(),
                 name: name,
+                ticketNameKey: ticketName.toLowerCase(),
                 ticketName: ticketName,
+                orderKey: orderKey.toLowerCase(),
                 barcode: barcode,
                 scanned: scanned,
                 scannedDateTime: scannedDateTime
@@ -275,6 +559,7 @@ DatabaseBuilder.prototype = {
                 self.addRow(
                     names[Math.floor(Math.random() * names.length)],
                     ticketTypes[Math.floor(Math.random() * ticketTypes.length)],
+                    self.generateOrderKey(),
                     self.generateBarcode()
                 ).then(function () {
                     console.log(i+'/'+numberOfTickets);
@@ -287,8 +572,7 @@ DatabaseBuilder.prototype = {
         });
     },
 
-    printTicketInfo: function (ticket)
-    {
+    printTicketInfo: function (ticket) {
         consoleWindow.writeLineToConsole(
             'Found ticket ' + ticket.ticketName + ' with barcode ' +
             ticket.barcode + ' for ' + ticket.name + ', scanned: '+
@@ -298,8 +582,7 @@ DatabaseBuilder.prototype = {
         );
     },
 
-    scanBarcode: function (barcode, ticketObjectStore, btn)
-    {
+    scanBarcode: function (barcode, ticketObjectStore, btn) {
         var self = this;
 
         return new Promise(function (fulfill, reject) {
@@ -377,8 +660,7 @@ DatabaseBuilder.prototype = {
         });
     },
 
-    getRandomBarcode: function ()
-    {
+    getRandomBarcode: function () {
         var self = this;
 
         return new Promise(function (fulfill, reject) {
@@ -404,13 +686,15 @@ DatabaseBuilder.prototype = {
         });
     },
 
-    generateBarcode: function ()
-    {
+    generateOrderKey: function () {
+        return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 8);
+    },
+
+    generateBarcode: function () {
         return this.generateRandomInt(10000000000000, 99999999999999).toString();
     },
 
-    generateRandomInt: function (min, max)
-    {
+    generateRandomInt: function (min, max) {
         return (Math.floor(
             Math.random() * (max - min + 1)
         ) + min)
